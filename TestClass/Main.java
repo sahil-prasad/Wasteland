86a6036c8b4c919416a99423054e69f796fe668f
Sahil <05sahilprasad@gmail.com>
2024-01-06 22:30:56 +0530

    Add

---
+++ TestClass/Main.java
@@ -0,0 +1,9 @@
+package TestClass;
+
+public class Main {
+    public static void main(String[]args){
+        Smartphone phone =new Smartphone();
+        System.out.println(phone.getBrand());
+    }
+    
+}
---
+++ TestClass/Smartphone.java
@@ -0,0 +1,19 @@
+package TestClass;
+
+public class Smartphone {
+    private String brand="Apple";
+
+    
+
+    public Smartphone(String brand) {
+        this.brand = brand;
+    }
+
+
+
+    public String getBrand() {
+        return brand;
+    }
+    
+    
+}
